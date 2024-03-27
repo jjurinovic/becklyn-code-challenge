82cb1f8e8cf9d55ca0cd12e9b5841924d9579b57
Juro Jurinovic <jjurinovic@gmail.com>
2024-03-27 19:50:20 +0100

    added enviroment variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    CONTENTFUL_SPACE_ID: 'h4fy7qjn6mui',
+    CONTENTFUL_ENVIROMENT: 'interview',
+    CONTENTFUL_ACCESS_TOKEN: 'aQQ9uKwj1LyR9J73YaDbgv0XQwhhpnr4tcsBR2D5LF0',
+  },
+};
 
 export default nextConfig;
