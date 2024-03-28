4fef24e713d53b091304fd6114e457bea7598326
Juro Jurinovic <jjurinovic@gmail.com>
2024-03-28 13:57:18 +0100

    add hover effect for job link and comments

--- src/app/ui/career/job.tsx
+++ src/app/ui/career/job.tsx
@@ -5,17 +5,29 @@ export const Job = ({ job }: { job: Job }) => {
   return (
     <div className='rounded-2xl bg-grey-50 border border-grey-200 px-6 py-6 md:py-8 w-full max-w-4xl mx-auto'>
       <div className='flex items-center justify-between text-sm text-primary-600 font-bold mb-2'>
-        {job.department.fields.title}
-        <a href='#' className='flex items-center justify-between text-base'>
-          <span className='hidden md:inline-block mr-3.5'>Stelle Anzeigen</span>
+        {/* Department */}
+        <span>{job.department.fields.title}</span>
+
+        {/* Show more link */}
+        <a
+          href='#'
+          className='flex items-center justify-between text-base group'
+        >
+          <span className='hidden md:inline-block mr-3.5 transition group-hover:translate-x-2'>
+            Stelle Anzeigen
+          </span>
           <ArrowUpRightIcon className='h-5 w-5'></ArrowUpRightIcon>
         </a>
       </div>
+
+      {/* Job name */}
       <p className='text-lg text-grey-900 font-bold mb-5'>{job?.name}</p>
+
+      {/* Location and type */}
       <div className='font-normal text-grey-700 flex items-center'>
         <MapPinIcon className='h-6 w-6 mr-2'></MapPinIcon>
         <span className='mr-6'>
-          {job.locations.map((loc) => loc.fields.city).join(', ')}
+          {job.locations.map((loc: any) => loc.fields.city).join(', ')}
         </span>
         <ClockIcon className='h-6 w-6 mr-2'></ClockIcon>
         <span>{job.type.fields.title}</span>
